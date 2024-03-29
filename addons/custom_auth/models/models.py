9949426cd5dc5b3706f13bd9d4af559ce2730670
namnam <namnam@gmail.com>
2024-03-29 10:19:35 +0700

    hêlo world

--- addons/custom_auth/models/models.py
+++ addons/custom_auth/models/models.py
@@ -1,21 +1,3 @@
-#
-# from odoo import api, fields, models
-#
-# class CustomUser(models.Model):
-#     _name = 'custom.user'
-#     _description = 'Custom User'
-#
-#     login = fields.Char(string='Login', required=True)
-#     password = fields.Char(string='Password', required=True)
-#
-#     @api.constrains('login')
-#     def _check_unique_login(self):
-#         if self.search_count([('login', '=', self.login)]) > 1:
-#             raise ValueError("Login must be unique.")
-#
-#     is_admin = fields.Boolean(string='Is Admin', default=False)
-
-# models/models.py
 
 from odoo import api, fields, models
 
@@ -25,11 +7,29 @@ class CustomUser(models.Model):
 
     login = fields.Char(string='Login', required=True)
     password = fields.Char(string='Password', required=True)
-    is_administrator = fields.Boolean(string='Administrator', default=False)
-    is_member = fields.Boolean(string='Member', default=True)
-    is_intern = fields.Boolean(string='Intern', default=False)
 
     @api.constrains('login')
     def _check_unique_login(self):
         if self.search_count([('login', '=', self.login)]) > 1:
             raise ValueError("Login must be unique.")
+
+    is_admin = fields.Boolean(string='Is Admin', default=False)
+
+# models/models.py
+
+# from odoo import api, fields, models
+#
+# class CustomUser(models.Model):
+#     _name = 'custom.user'
+#     _description = 'Custom User'
+#
+#     login = fields.Char(string='Login', required=True)
+#     password = fields.Char(string='Password', required=True)
+#     is_administrator = fields.Boolean(string='Administrator', default=False)
+#     is_member = fields.Boolean(string='Member', default=True)
+#     is_intern = fields.Boolean(string='Intern', default=False)
+#
+#     @api.constrains('login')
+#     def _check_unique_login(self):
+#         if self.search_count([('login', '=', self.login)]) > 1:
+#             raise ValueError("Login must be unique.")
